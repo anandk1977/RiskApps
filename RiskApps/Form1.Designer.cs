60d4cc840a4e82ffdc1313b0801536d60d08b6ea
anandk1977 <anandk77@yahoo.com>
2016-06-11 19:22:11 +1200

    Creation of UI

--- RiskApps/Form1.Designer.cs
+++ RiskApps/Form1.Designer.cs
@@ -35,19 +35,77 @@ namespace RiskApps
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabRiskApps = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabRiskApps.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tabRiskApps
+            // 
+            this.tabRiskApps.Controls.Add(this.tabPage1);
+            this.tabRiskApps.Controls.Add(this.tabPage2);
+            this.tabRiskApps.Location = new System.Drawing.Point(30, 56);
+            this.tabRiskApps.Name = "tabRiskApps";
+            this.tabRiskApps.SelectedIndex = 0;
+            this.tabRiskApps.Size = new System.Drawing.Size(826, 465);
+            this.tabRiskApps.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(818, 439);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Selected Bets";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(818, 439);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "UnSelected Bets";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(18, 93);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(657, 242);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1027, 545);
+            this.ClientSize = new System.Drawing.Size(1027, 598);
+            this.Controls.Add(this.tabRiskApps);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Risk Application";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabRiskApps.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabRiskApps;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+
     }
 }
--- RiskApps/Form1.cs
+++ RiskApps/Form1.cs
@@ -15,12 +15,26 @@ namespace RiskApps
     using System.Text;
     using System.Threading.Tasks;
     using System.Windows.Forms;
+    using System.Diagnostics.CodeAnalysis;
+
+  
+    [SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1513:ClosingCurlyBracketMustBeFollowedByBlankLine", Justification = "There are no issues with this code")]
 
     public partial class Form1 : Form
     {
         public Form1()
         {
             this.InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
